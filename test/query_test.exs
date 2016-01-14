1d720f07f95b7047cbe533f350b614a2db4b14fc
Thiago Correa <thiagobocorrea@gmail.com>
2016-01-14 08:34:53 -0200

    more tests
    still need to fix get tests

--- test/query_test.exs
+++ test/query_test.exs
@@ -1,40 +1,65 @@
 defmodule QueryTest do
   use ExUnit.Case
 
-  setup_all do
+  setup do
     Application.put_env(:rethinkdb_ecto_test, TestRepo, [])
 
     {:ok, conn} = RethinkDB.Connection.start_link
     {:ok, _} = TestRepo.start_link
     RethinkDB.Query.table_create("posts") |> RethinkDB.Connection.run(conn)
+    {:ok, model} = TestRepo.insert(%TestModel{title: "yay"})
 
-    {:ok, conn: conn}
+    {:ok, model: model}
   end
 
-  test "get, insert and update queries work" do
-    {:ok, test_model} = TestRepo.insert(%TestModel{title: "yay"})
-    from_db = TestRepo.get(TestModel, test_model.id)
-    assert test_model == from_db
-
-    # update_changeset = TestRepo.changeset(from_db, %{title: "yayay"})
-    # {:ok, updated_model} = TestRepo.update(update_changeset)
-    # assert updated_model.title == "yayay"
+  test "insert queries work", %{model: model} do
+    assert model.title == "yay"
   end
-
-  # test "respect schema" do
-  #   {:error, changeset} = TestRepo.insert(%TestModel{blablabla: "yay"})
-  #   assert changeset.errors == "blablabla"
-  # end
 
   test "insert queries with Ecto.Date should work" do
     date = Ecto.Date.utc
-    {:ok, test_model} = TestRepo.insert(%TestModel{date: date})
-    assert test_model.date == date
+    {:ok, model} = TestRepo.insert(%TestModel{date: date})
+    assert model.date == date
   end
 
   test "insert queries with Ecto.DateTime should work" do
     date = Ecto.DateTime.utc
-    {:ok, test_model} = TestRepo.insert(%TestModel{date: date})
-    assert test_model.date == date
+    {:ok, model} = TestRepo.insert(%TestModel{date: date})
+    assert model.date == date
+  end
+
+  test "get one queries work", %{model: model} do
+    from_db = TestRepo.get(TestModel, model.id)
+    assert model == from_db
+  end
+
+  # test "get many queries work", %{model: model}  do
+  #   {:ok, model_2} = TestRepo.insert(%TestModel{title: "yayay"})
+  #   from_db = TestRepo.all(TestModel)
+  #   assert from_db == [model, model_2]
+  # end
+
+  # test "filtered queries work", %{model: model}  do
+  #   {:ok, model_2} = TestRepo.insert(%TestModel{title: "yayay"})
+
+  #   query = table("posts") |>
+  #     filter(fn (post) ->
+  #       post[:title] == "yayay"
+  #     end)
+
+  #   from_db = TestRepo.query(TestModel, query)
+  #   assert from_db.title == "yayay"
+  # end
+
+  test "update queries work", %{model: model} do
+    update_changeset = TestRepo.changeset(model, %{title: "yayay"})
+    {:ok, updated_model} = TestRepo.update(update_changeset)
+    assert updated_model.title == "yayay"
+  end
+
+  test "delete queries work", %{model: model} do
+    TestRepo.delete(model)
+    from_db = TestRepo.get(TestModel, model.id)
+    refute from_db
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,14 @@
 defmodule TestRepo do
   use RethinkDB.Ecto.Repo, otp_app: :rethinkdb_ecto_test
+  import Ecto.Changeset
+
+  @required_fields ~w(title)
+  @optional_fields ~w(content user date)
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
 end
 
 defmodule TestModel do
